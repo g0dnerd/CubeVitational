3904e1ad50ac05e9bb9d7193fe1792c1eef26f6f
Ingo Kraft <drifternews@googlemail.com>
2022-02-16 22:58:28 +0100

    Refactoring to OOP and ASCII added to Menus

---
+++ pod.py
@@ -0,0 +1,49 @@
+import copy
+import csv
+import random
+from operator import getitem
+from pypair import Tournament
+from collections import OrderedDict
+from printService import PrintService
+
+printService = PrintService()
+
+
+class Pod():
+    roundNumber = 1
+    playerList  = []
+    currentPairings = []
+    current_results = []
+
+    def randomize_seating(self):
+        # print("Unrandomized group: ", Pod.playerList)
+        # printService.print_table(self)
+        tempPlayerList = copy.deepcopy(Pod.playerList)
+        [random.shuffle(sublist) for sublist in tempPlayerList]
+        for i in range(len(Pod.playerList[0])):
+            tempPlayerList[0][i] = Pod.playerList[0][Pod.playerList[1].index(tempPlayerList[1][i])]
+
+        # print("\n Randomized seatings for this group: ", tempPlayerList[1])
+        Pod.playerList = tempPlayerList
+        printService.print_table(self)
+
+    def load_players(self):
+        listToFill = [[] for _ in range(2)]
+        # csvPath = input("Please enter the full name of the relevant CSV file: ")
+        # print("Please enter the full name of the relevant CSV file: ")
+        csvPath = 'playerlist1.csv'
+        with open(csvPath) as csvfile:
+            playerReader = csv.reader(csvfile, delimiter=',')
+            for player in playerReader:
+                # skip the row with headers
+                if player[0] != 'ID:':
+                    listToFill[0].append(int(player[0]))
+                    listToFill[1].append(player[1])
+
+        Pod.playerList = listToFill
+        
+
+    def new_pairings(self, new_pairings):
+        Pod.currentPairings = new_pairings
+        Pod.current_results = ["MISSING"] * len(Pod.currentPairings)
+        
