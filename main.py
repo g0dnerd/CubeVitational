15d746f7f065f76554e17d88c188da70231f459d
Ingo Kraft <drifternews@googlemail.com>
2022-02-17 14:14:21 +0100

    Moved Tournament (to) to Pod Class

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-# pypair tournament maker branch for the CubeVitational Hamburg,
-# a cube tournament with the following tournament structure:
-# 16 people in two groups of 8 play two drafts each.
-# these drafts are 3 rounds each and seatings are re-done after the fist pod.
-# # After both drafts, a cut to top 8 over both groups is made.
-
 from pypair import Tournament
 from printService import PrintService
 from pod import Pod
 
 # USER-DEFINED VARIABLES
-ROUND_NUMBER = 6
+ROUND_NUMBER = 3
 
 # Define Classes 
 printService = PrintService()
 pod = Pod()
-to = Tournament()
-
 
 ###################### Reporting ######################
 def get_nth_key(dictionary, n=0):
@@ -41,12 +33,11 @@ def report_results(pairings_, playerList_):
                 result = input('Enter result %s vs %s in W-L-D format: ' % (player1, player2))
                 resultList = list()
                 try:
-                    pod.currentResults.insert(tableNumber - 1, result)
-                    print(type(result[0]))
+                    pod.current_results.insert(tableNumber - 1, result)
                     resultList.append(result[0])
                     resultList.append(result[2])
                     resultList.append(result[4])
-                    to.report_match(tableNumber, resultList)
+                    pod.to.report_match(tableNumber, resultList)
                 except IndexError:
                     print("Error: Result must be entered in W-L-D formatting (e.g. 2-1-0)")
             except ValueError:
@@ -67,44 +58,18 @@ def main():
     # Randomize seatings
     pod.randomize_seating()
 
-    # Add Players to Tournament
-    for p in range(len(pod.playerList[0])):
-        to.add_player(pod.playerList[0][p], pod.playerList[1][p], False)
-
     # Start Tournament
-    for round_ in range(3):
-        pod.new_pairings(to.pair_round())
+    for round_ in range(ROUND_NUMBER):
+        pod.new_pairings()
         printService.print_pairings(pod)
 
         report_results(pod.currentPairings, pod.playerList)
 
         # Print out current standings
-        printService.print_standings(to, pod)
+        printService.print_standings(pod)
         pod.roundNumber += 1
 
-    # Initialize the second pod for the group with new random seatings
-    toTwo = Tournament()
-    podTwo = Pod()
-    podTwo.load_players()
-    podTwo.randomize_seating()
-
-    # Add Players to Tournament
-    for p in range(len(podTwo.playerList[0])):
-        toTwo.add_player(podTwo.playerList[0][p], podTwo.playerList[1][p], False)
-
-    print(podTwo.playerList)
-
-    for round_ in range(3):
-        podTwo.new_pairings(toTwo.pair_round())
-        # printService.print_pairings(podTwo)
-
-        report_results(podTwo.currentPairings, podTwo.playerList)
-
-        # Print out current standings
-        printService.print_standings(toTwo, podTwo)
-        podTwo.roundNumber += 1
-
-    printService.print_merged_standings(toTwo, podTwo, to)
+        # Repeat till Finish
 
 
 if __name__ == "__main__":
